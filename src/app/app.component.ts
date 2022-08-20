e2fa02c005ca7919e3b16da1aa092f140e3195ac
Valentine Egorov <vvyego15@mts.ru>
2022-08-20 18:48:06 +0300

    Add translations, and distinctUntilChanged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { DataSource } from '@angular/cdk/collections';
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { map, race, repeat, Subject, timer } from 'rxjs';
+import { distinctUntilChanged, map, race, repeat, Subject, timer } from 'rxjs';
 import { CbrRatesService } from './cbr-rates.service';
 import { Valute } from './shared/interfaces/valute.interface';
 
@@ -17,12 +17,22 @@ export class AppComponent {
   readonly displayedColumns: (keyof Valute)[] = ['Name', 'Value'];
   readonly refreshRates$: Subject<void> = new Subject<void>();
   readonly ratesDataSource: DataSource<Valute> = new MatTableDataSource();
+  readonly columnNamesMap: Record<keyof Valute, string> = {
+    CharCode: 'Букв. код',
+    ID: 'ID',
+    Name: 'Валюта',
+    Nominal: 'Единиц',
+    NumCode: 'Числ. код',
+    Previous: 'Пред. курс',
+    Value: 'Курс'
+  }
 
   constructor(private cbrRates: CbrRatesService) {
     this.ratesDataSource.connect = () => this.cbrRates.getRates().pipe(
       repeat({
         delay: () => race([this.refreshRates$, timer(AUTO_REFRESH_INTERVAL_MS)]).pipe(repeat())
       }),
+      distinctUntilChanged((v1, v2) => v1.Timestamp === v2.Timestamp),
       map(rates => Object.values(rates.Valute))
     );
   }
