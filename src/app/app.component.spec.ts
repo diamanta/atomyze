621dbae85b3527eb1508ace94c9a0d451400da0f
Valentine Egorov <vvyego15@mts.ru>
2022-08-23 02:28:37 +0300

    Done with some component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,31 @@
 import { HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
+import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Observable, of } from 'rxjs';
 import { AppComponent } from './app.component';
+import { Valute } from './interfaces/valute.interface';
+import { valuteResponseMock } from './mocks/valute-response.mock';
+import { CalculateExchangePipe } from './pipes/calculate-exchange.pipe';
+import { CbrRatesService } from './services/cbr-rates.service';
+
+@Injectable()
+class MockCbrService extends CbrRatesService {
+  override getValute(): Observable<Valute[]> {
+    return of(valuteResponseMock);
+  }
+}
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
-  let app: AppComponent;
+  let appComponent: AppComponent;
+  let mockCbrService: MockCbrService
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -23,20 +38,52 @@ describe('AppComponent', () => {
         FormsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        CalculateExchangePipe
       ],
+      providers: [
+        {
+          provide: CbrRatesService,
+          useClass: MockCbrService
+        }
+      ]
     }).compileComponents();
     fixture = TestBed.createComponent(AppComponent);
-    app = fixture.componentInstance;
+    mockCbrService = TestBed.inject(CbrRatesService)
+    appComponent = fixture.componentInstance;
+    fixture.detectChanges();
+
   });
 
   it('should create the app', () => {
-    expect(app).toBeTruthy();
+    expect(appComponent).toBeTruthy();
   });
 
   it('should render table', () => {
-    fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('div.container')?.firstElementChild?.tagName).toEqual('mat-table'.toUpperCase());
+    expect(compiled.querySelector('div.container .rates-table')).toBeTruthy();
   });
+
+  it('should render table header', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const header = compiled.querySelector('div.container .rates-table .rates-table-header')
+    expect(header).toBeTruthy();
+    expect(header?.childElementCount).toEqual(appComponent.displayedColumns.length);
+    appComponent.displayedColumns.forEach((column, idx) => {
+      expect(header?.children.item(idx)?.textContent).toEqual(appComponent.columnNamesMap[column]);
+    })
+  })
+
+  it('should render table input', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const input = compiled.querySelector('div.container .rates-table .rates-table-header .exchange-input');
+    expect(input).toBeTruthy();
+  })
+
+  it('should render rows', async () => {
+    const tableComponent = fixture.debugElement.query(By.css('mat-table'));
+    await fixture.whenRenderingDone();
+    expect(tableComponent.queryAll(By.css('mat-row')).length).toEqual(valuteResponseMock.length);
+  })
+
 });
---
+++ src/app/mocks/valute-response.mock.ts
@@ -0,0 +1,310 @@
+import { Valute } from '../interfaces/valute.interface';
+
+export const valuteResponseMock: Valute[] = [
+  {
+    "ID": "R01010",
+    "NumCode": "036",
+    "CharCode": "AUD",
+    "Nominal": 1,
+    "Name": "Австралийский доллар",
+    "Value": 41.1741,
+    "Previous": 40.8839
+  },
+  {
+    "ID": "R01020A",
+    "NumCode": "944",
+    "CharCode": "AZN",
+    "Nominal": 1,
+    "Name": "Азербайджанский манат",
+    "Value": 35.1423,
+    "Previous": 34.7836
+  },
+  {
+    "ID": "R01035",
+    "NumCode": "826",
+    "CharCode": "GBP",
+    "Nominal": 1,
+    "Name": "Фунт стерлингов Соединенного королевства",
+    "Value": 70.5253,
+    "Previous": 70.9763
+  },
+  {
+    "ID": "R01060",
+    "NumCode": "051",
+    "CharCode": "AMD",
+    "Nominal": 100,
+    "Name": "Армянских драмов",
+    "Value": 14.7344,
+    "Previous": 14.5951
+  },
+  {
+    "ID": "R01090B",
+    "NumCode": "933",
+    "CharCode": "BYN",
+    "Nominal": 1,
+    "Name": "Белорусский рубль",
+    "Value": 23.3943,
+    "Previous": 23.0041
+  },
+  {
+    "ID": "R01100",
+    "NumCode": "975",
+    "CharCode": "BGN",
+    "Nominal": 1,
+    "Name": "Болгарский лев",
+    "Value": 30.7109,
+    "Previous": 30.7723
+  },
+  {
+    "ID": "R01115",
+    "NumCode": "986",
+    "CharCode": "BRL",
+    "Nominal": 1,
+    "Name": "Бразильский реал",
+    "Value": 11.4981,
+    "Previous": 11.4221
+  },
+  {
+    "ID": "R01135",
+    "NumCode": "348",
+    "CharCode": "HUF",
+    "Nominal": 100,
+    "Name": "Венгерских форинтов",
+    "Value": 14.7416,
+    "Previous": 14.6635
+  },
+  {
+    "ID": "R01200",
+    "NumCode": "344",
+    "CharCode": "HKD",
+    "Nominal": 10,
+    "Name": "Гонконгских долларов",
+    "Value": 76.2792,
+    "Previous": 75.5006
+  },
+  {
+    "ID": "R01215",
+    "NumCode": "208",
+    "CharCode": "DKK",
+    "Nominal": 10,
+    "Name": "Датских крон",
+    "Value": 80.7607,
+    "Previous": 80.9053
+  },
+  {
+    "ID": "R01235",
+    "NumCode": "840",
+    "CharCode": "USD",
+    "Nominal": 1,
+    "Name": "Доллар США",
+    "Value": 59.7419,
+    "Previous": 59.1321
+  },
+  {
+    "ID": "R01239",
+    "NumCode": "978",
+    "CharCode": "EUR",
+    "Nominal": 1,
+    "Name": "Евро",
+    "Value": 59.5344,
+    "Previous": 59.3934
+  },
+  {
+    "ID": "R01270",
+    "NumCode": "356",
+    "CharCode": "INR",
+    "Nominal": 100,
+    "Name": "Индийских рупий",
+    "Value": 74.9574,
+    "Previous": 74.1923
+  },
+  {
+    "ID": "R01335",
+    "NumCode": "398",
+    "CharCode": "KZT",
+    "Nominal": 100,
+    "Name": "Казахстанских тенге",
+    "Value": 12.5395,
+    "Previous": 12.4243
+  },
+  {
+    "ID": "R01350",
+    "NumCode": "124",
+    "CharCode": "CAD",
+    "Nominal": 1,
+    "Name": "Канадский доллар",
+    "Value": 45.9871,
+    "Previous": 45.7396
+  },
+  {
+    "ID": "R01370",
+    "NumCode": "417",
+    "CharCode": "KGS",
+    "Nominal": 100,
+    "Name": "Киргизских сомов",
+    "Value": 73.9838,
+    "Previous": 73.0206
+  },
+  {
+    "ID": "R01375",
+    "NumCode": "156",
+    "CharCode": "CNY",
+    "Nominal": 10,
+    "Name": "Китайских юаней",
+    "Value": 87.1277,
+    "Previous": 86.583
+  },
+  {
+    "ID": "R01500",
+    "NumCode": "498",
+    "CharCode": "MDL",
+    "Nominal": 10,
+    "Name": "Молдавских леев",
+    "Value": 31.0573,
+    "Previous": 30.7491
+  },
+  {
+    "ID": "R01535",
+    "NumCode": "578",
+    "CharCode": "NOK",
+    "Nominal": 10,
+    "Name": "Норвежских крон",
+    "Value": 61.1177,
+    "Previous": 60.3943
+  },
+  {
+    "ID": "R01565",
+    "NumCode": "985",
+    "CharCode": "PLN",
+    "Nominal": 1,
+    "Name": "Польский злотый",
+    "Value": 12.5455,
+    "Previous": 12.5137
+  },
+  {
+    "ID": "R01585F",
+    "NumCode": "946",
+    "CharCode": "RON",
+    "Nominal": 1,
+    "Name": "Румынский лей",
+    "Value": 12.2387,
+    "Previous": 12.2055
+  },
+  {
+    "ID": "R01589",
+    "NumCode": "960",
+    "CharCode": "XDR",
+    "Nominal": 1,
+    "Name": "СДР (специальные права заимствования)",
+    "Value": 78.2075,
+    "Previous": 77.874
+  },
+  {
+    "ID": "R01625",
+    "NumCode": "702",
+    "CharCode": "SGD",
+    "Nominal": 1,
+    "Name": "Сингапурский доллар",
+    "Value": 42.8965,
+    "Previous": 42.6362
+  },
+  {
+    "ID": "R01670",
+    "NumCode": "972",
+    "CharCode": "TJS",
+    "Nominal": 10,
+    "Name": "Таджикских сомони",
+    "Value": 58.2694,
+    "Previous": 57.6645
+  },
+  {
+    "ID": "R01700J",
+    "NumCode": "949",
+    "CharCode": "TRY",
+    "Nominal": 10,
+    "Name": "Турецких лир",
+    "Value": 33.0193,
+    "Previous": 32.8786
+  },
+  {
+    "ID": "R01710A",
+    "NumCode": "934",
+    "CharCode": "TMT",
+    "Nominal": 1,
+    "Name": "Новый туркменский манат",
+    "Value": 17.0691,
+    "Previous": 16.8949
+  },
+  {
+    "ID": "R01717",
+    "NumCode": "860",
+    "CharCode": "UZS",
+    "Nominal": 10000,
+    "Name": "Узбекских сумов",
+    "Value": 54.7236,
+    "Previous": 54.0761
+  },
+  {
+    "ID": "R01720",
+    "NumCode": "980",
+    "CharCode": "UAH",
+    "Nominal": 10,
+    "Name": "Украинских гривен",
+    "Value": 16.1768,
+    "Previous": 16.0997
+  },
+  {
+    "ID": "R01760",
+    "NumCode": "203",
+    "CharCode": "CZK",
+    "Nominal": 10,
+    "Name": "Чешских крон",
+    "Value": 24.3894,
+    "Previous": 24.455
+  },
+  {
+    "ID": "R01770",
+    "NumCode": "752",
+    "CharCode": "SEK",
+    "Nominal": 10,
+    "Name": "Шведских крон",
+    "Value": 56.2271,
+    "Previous": 56.1927
+  },
+  {
+    "ID": "R01775",
+    "NumCode": "756",
+    "CharCode": "CHF",
+    "Nominal": 1,
+    "Name": "Швейцарский франк",
+    "Value": 62.2895,
+    "Previous": 61.7632
+  },
+  {
+    "ID": "R01810",
+    "NumCode": "710",
+    "CharCode": "ZAR",
+    "Nominal": 10,
+    "Name": "Южноафриканских рэндов",
+    "Value": 35.0148,
+    "Previous": 35.0064
+  },
+  {
+    "ID": "R01815",
+    "NumCode": "410",
+    "CharCode": "KRW",
+    "Nominal": 1000,
+    "Name": "Вон Республики Корея",
+    "Value": 44.5902,
+    "Previous": 44.5977
+  },
+  {
+    "ID": "R01820",
+    "NumCode": "392",
+    "CharCode": "JPY",
+    "Nominal": 100,
+    "Name": "Японских иен",
+    "Value": 43.5437,
+    "Previous": 43.3902
+  }
+]
